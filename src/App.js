5006d11b8d16a59be013d8eb65e25dd58a5c1ffc
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-04-20 01:11:07 +0700

    make and call api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import React, { useEffect, useState } from 'react'
+
+import { API } from 'aws-amplify'
+
 function App() {
+  const [coins, updateCoins] = useState([])
+  console.log(coins)
+
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello from AWS Amplify</h1>
-      <AmplifySignOut />
+    <div className='App'>
+      {coins.map((coin, index) => (
+        <div key='index'>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default withAuthenticator(App)
+export default App
