2ef6c21f857cdb08803eddf132645e8e81e44a3e
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-04-20 11:37:58 +0700

    finish chapter 2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import { API } from 'aws-amplify'
 
 function App() {
   const [coins, updateCoins] = useState([])
-  console.log(coins)
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value })
+  }
 
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins')
-    updateCoins(data.coins)
+    const { limit, start } = input
+    const data = await API.get(
+      'cryptoapi',
+      `/coins?limit=${limit}&start=${start}`
+    )
+    updateCoins(data.coins.data)
   }
 
   useEffect(() => {
@@ -18,14 +26,25 @@ function App() {
 
   return (
     <div className='App'>
-      {coins.map((coin, index) => (
-        <div key='index'>
-          <h2>
-            {coin.name} - {coin.symbol}
-          </h2>
-          <h5>${coin.price_usd}</h5>
-        </div>
-      ))}
+      <input
+        onChange={(e) => updateInputValues('limit', e.target.value)}
+        placeholder='limit'
+      />
+      <input
+        placeholder='start'
+        onChange={(e) => updateInputValues('start', e.target.value)}
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
+
+      {coins &&
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>
+              {coin.name} - {coin.symbol}
+            </h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))}
     </div>
   )
 }
